7214c4d7e16c71865d805551b4f6f550730eae2f
Ryuya Naha <e205710@ie.u-ryukyu.ac.jp>
2021-12-19 14:02:43 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import jp.ac.urykyu.ie.e205710;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("main method runs");
+        chatBot chat1 = new chatBot("")
+    }
+}
